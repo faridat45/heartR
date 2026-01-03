b3f208e27941990688a6914139e66d02eec22e00
ciaraolo7 <ciara.olohan.2023@mumail.ie>
2026-01-03 22:29:27 +0000

    resolved test related errors

--- tests/testthat/test_summ_heartr.R
+++ tests/testthat/test_summ_heartr.R
@@ -1,13 +1,12 @@
 library(HeartR)
 
-test_that("summ_heartr has a valid output", {
-  expect_s3_class(summ_heartr(heart_dat$age), "summ_heartr")
-  expect_length(summ_heartr(heart_dat$restingBP), 9)
-  expect_identical(summ_heartr(heart_dat$age)$Mean, mean(heart_dat$age))
-  expect_identical(summ_heartr(heart_dat$oldpeak)$Median, median(heart_dat$oldpeak))
-  expect_identical(summ_heartr(heart_dat$cholesterol)$Var, var(heart_dat$cholesterol))
-  })
 
+test_that("summ_heartr has a valid output", {
+  expect_length(summ_heartr(heart_dat$restingBP), 9)
+  expect_identical(summ_heartr(heart_dat$age)$Mean, mean(dat$age))
+  expect_identical(summ_heartr(heart_dat$oldpeak)$Median, median(dat$oldpeak))
+  expect_identical(summ_heartr(heart_dat$cholesterol)$Var, var(dat$cholesterol))
+  })
 
 test_that("summ_heartr only takes a numeric input", {
   expect_error(summ_heartr(heart_dat))
